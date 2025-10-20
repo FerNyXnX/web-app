d45501cebe295e0cc8ccb0a90cf5eb96fa9e2f24
FerNyXnX <fernyxnx@gmail.com>
2025-10-19 21:39:22 -0700

    feat: configurar cliente Axios para TMDB API

---
+++ src/services/api.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { API_CONFIG } from '../utils/constants';
+
+const api = axios.create({
+  baseURL: API_CONFIG.BASE_URL,
+  params: {
+    api_key: API_CONFIG.API_KEY,
+    language: 'es-ES',
+  },
+});
+
+// Request interceptor
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      // Server responded with error
+      console.error('API Error:', error.response.data);
+    } else if (error.request) {
+      // Request made but no response
+      console.error('Network Error:', error.request);
+    } else {
+      // Something else happened
+      console.error('Error:', error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
